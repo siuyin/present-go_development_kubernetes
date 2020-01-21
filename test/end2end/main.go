00fc21efef539b5d8439da4a654dc8432940baa9
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-01-21 11:23:51 +0000

    Added end to end test.

---
+++ test/end2end/main.go
@@ -0,0 +1,42 @@
+// 10 OMIT
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/siuyin/dflt"
+)
+
+// 20 OMIT
+func main() {
+	endp := dflt.EnvString("ENDPOINT", "http://192.168.39.230:31051/")
+	fmt.Println("End to end testing.\n")
+	fmt.Printf("getting %s\n", endp)
+	resp, err := http.Get(endp)
+	if err != nil {
+		log.Fatalf("could not reach endpoint: %s: %v", endp, err)
+	}
+
+	fmt.Println("checking status code")
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("bad status code received: %v", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+	// 30 OMIT
+	fmt.Println("checking response")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("error reading body: %v\n", err)
+	}
+	if !bytes.Contains(body, []byte("Hello, the time is")) {
+		log.Fatalf("unexpected body: %s\n", body)
+	}
+
+	fmt.Printf("\ntests complete.\n")
+}
+
+// 40 OMIT
